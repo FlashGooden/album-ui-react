cabcd100f1d4c532e889c21801f8f4e1a792bcee
FlashGooden <Tro12185@gmail.com>
2022-02-25 00:15:28 -0500

    feat: add search component

---
+++ src/Components/Search/Search.js
@@ -0,0 +1,8 @@
+import './Search.scss'
+import React from 'react'
+
+export default function Search() {
+  return (
+    <input type="text" placeholder="Search by name..." className='search'></input>
+  )
+}
