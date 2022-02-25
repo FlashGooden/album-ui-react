adda1ca590c9353de3243b4821b4c49275c2bab9
FlashGooden <Tro12185@gmail.com>
2022-02-25 00:14:13 -0500

    feat: change link to anchor tag

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -9,7 +9,7 @@ export default function User({userName, company, website, albumCount, userPhoto}
       <div className='user__info'>
         <p className="user__name">{userName}</p>
         <p className="user__company">{company}</p>
-        <link rel="stylesheet" href={website} className="user__website" />
+        <a rel="stylesheet" href={website} className="user__website" >{website}</a>
         <p className="user__album">{albumCount}</p>
       </div>
     </div>
