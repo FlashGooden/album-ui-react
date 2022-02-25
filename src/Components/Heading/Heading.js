74bc90bbd68edb70219fcb204de418b27fc6fd0e
FlashGooden <Tro12185@gmail.com>
2022-02-24 22:49:31 -0500

    feat: import image into header + update to sass

--- src/Components/Heading/Heading.js
+++ src/Components/Heading/Heading.js
@@ -1,7 +1,9 @@
+import './Heading.scss'
 import React from 'react'
+import logo from '../../assets/Logo.png'
 
 export default function Heading() {
   return (
-    <img alt='Cadence Lab' className='heading' src={null}/>
+    <img alt='Cadence Lab' className='heading' src={logo}/>
   )
 }
