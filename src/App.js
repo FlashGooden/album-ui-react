01460e4042443be4ace0787398a4dcb75f706dd1
FlashGooden <Tro12185@gmail.com>
2022-02-27 01:28:56 -0500

    feat: fetch Users and import into App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.scss';
+import { useState, useEffect } from 'react';
 import Heading from './Components/Heading/Heading'
 import SearchForm from './Components/Search/Search';
 import UserListContainer from './Components/UserListContainer/UserListContainer';
+import {fetchAlbums, fetchPhotos, fetchUsers}from './Api/Api'
 
 function App() {
+
+  const [albums, setAlbums] = useState([])
+  const [photos, setPhotos] = useState([])
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetchAlbums().then(setAlbums).then(console.log)
+    fetchPhotos().then(setPhotos).then(console.log)
+    fetchUsers().then(setUsers).then(console.log)
+
+  },[albums, photos, users]);
+
   return (
     <div className="App">
       <Heading />
